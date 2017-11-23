7bd5862be8ebe5a94da2375b14ef59c8f4076632
Cristiane Ferreira <cristianedeferreira@gmail.com>
2017-11-23 21:07:25 -0200

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = 3000
+
+//express setup
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static('public'))
+
+//templates
+app.set('views', path.join(__dirname, 'public'))
+app.set('view engine', 'ejs')
+
+app.get('/', (request, response) => {
+    const data = { vehicle:null, submitted:false }
+    response.render('index', data)
+})
+
+app.get('/status', (request, response) => {
+    const { getVehicleInformations } = require('./app/check-vehicle')
+    vehicleStatus =  getVehicleInformations(request.query.vehiclePlate)
+    
+    const data = {
+        vehicle: vehicleStatus,
+        submitted: true
+    }
+
+    response.render('index', data)
+})
+
+app.listen(PORT, () => { 
+    console.log('Server is running on port ' + PORT)
+})
