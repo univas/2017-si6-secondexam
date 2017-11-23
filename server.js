0cf54ce70dd6fe5d0754b6b302ae2708ad2b416c
EGGabriel <ps.eggabriel@gmail.com>
2017-11-23 21:24:35 -0200

    change PORT to 3000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const PORT = 3002
+const PORT = 3000
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
